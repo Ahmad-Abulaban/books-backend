e5bdabf6748af831096944f82bce49cb6790f61e
Ahmad-Abulaban <eng.ahmad090@gmail.com>
2021-09-28 22:38:52 +0300

    still have some error

--- Module/book.js
+++ Module/book.js
@@ -52,10 +52,31 @@ function seedBookInformation() {
 
 // seedBookInformation();
 
-// http://localhost:3001/books?emailin=
+
+
+async function addBookHandler(req,res){
+    let { title, description, status, email } = req.body;
+    await BooksModel.create({
+        title,
+        description,
+        status,
+        email
+    })
+
+    BooksModel.find({},(error,data) => {
+        if(error) {
+            console.log('error in getting data',error);
+        } else {
+            res.send(data);
+        }
+    })
+}
+
+
+// http://localhost:3001/books
 function getBooksHandler(req,res){
-    let emailin = req.query.emailin
-    BooksModel.find({email:emailin},function(error,Data) {
+    // let emailin = req.query.emailin
+    BooksModel.find({},(error,Data)=> {
         if(error) {
             console.log('error in getting data',error)
         } else {
@@ -64,5 +85,19 @@ function getBooksHandler(req,res){
     })
 }
 
+function deleteBookHandler(req, res) {
+    let {bookID} = req.query;
 
-module.exports = getBooksHandler;
+    BooksModel.deleteOne({ _id: bookID }).then(() => {
+        BooksModel.find({}, function (error, data) {
+            if (error) {
+                console.log('error in getting data', error)
+            } else {
+                res.send(data)
+            }
+        })
+    })
+}
+
+
+module.exports = getBooksHandler , addBookHandler , deleteBookHandler;
--- server.js
+++ server.js
@@ -2,17 +2,30 @@
 
 const express = require('express');
 const cors = require('cors');
-const getBooksHandler = require('./Module/book')
-
 require('dotenv').config();
+
+const addBookHandler = require('./Module/book')
+const getBooksHandler = require('./Module/book')
+const deleteBookHandler = require('./Module/book')
 
 const server = express();
 server.use(cors());
 const PORT = process.env.PORT;
 
 
+
+
 server.get('/', homeHandler);
 server.get('/books', getBooksHandler)
+server.post('/addBook', addBookHandler);
+server.delete('/deletebook', deleteBookHandler);
+
+
+server.get('*', notFoundHandler);
+
+function notFoundHandler(req, res) {
+    res.status(404).send('not found 404')
+}
 
 function homeHandler(req, res) {
     res.send('all good')
@@ -21,9 +34,3 @@ function homeHandler(req, res) {
 server.listen(PORT, ()=>{
     console.log(`Listening on PORT ${PORT}`)
 });
-
-server.get('*', notFoundHandler);
-
-function notFoundHandler(req, res) {
-    res.status(404).send('not found 404')
-}
